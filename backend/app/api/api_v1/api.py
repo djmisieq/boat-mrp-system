568daad972a705961c5e5ea9cf5407a5b403019d
djmisieq <zadroga.d@gmail.com>
2025-03-03 13:22:14 +0100

    Dodanie głównego routera API

---
+++ backend/app/api/api_v1/api.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+from app.api.api_v1.endpoints import auth, users, products, boms, orders
+
+api_router = APIRouter()
+api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(products.router, prefix="/products", tags=["products"])
+api_router.include_router(boms.router, prefix="/boms", tags=["boms"])
+api_router.include_router(orders.router, prefix="/orders", tags=["orders"])
